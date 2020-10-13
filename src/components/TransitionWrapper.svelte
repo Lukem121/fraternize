25df851445519247addc5afb5a0cd59af5b9c099
luke askew <lukeask@hotmail.co.uk>
2020-10-13 01:50:13 +0100

    👉 Started work on sign up

---
+++ src/components/TransitionWrapper.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { fly } from 'svelte/transition'; 
+    let show = true;
+</script>
+
+{#if show}
+    <div class="relative" in:fly="{{ x: -300, duration: 500, delay: 300 }}" out:fly="{{ x:300, duration:300 }}" >
+        <slot></slot>
+    </div>
+{/if}
+
+<style>
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
 	purge: false,
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'rashekgreen': '#50FA7B',
+				'rashekblack': '#202124',
+				'offwhite': '#E8E8E9',
+				'emojigold': '#FAC036',
+			},
+		},
+		fontFamily: {
+			'crete': ['Crete Round', 'serif'],
+		}
 	},
 	variants: {},
 	plugins: [],
