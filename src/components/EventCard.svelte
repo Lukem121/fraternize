4733d6b8887e151f1991874356d23d5f0f1bb920
luke askew <lukeask@hotmail.co.uk>
2020-10-16 02:37:55 +0100

    Fixed small bugs

--- src/components/EventCard.svelte
+++ src/components/EventCard.svelte
@@ -9,16 +9,17 @@
         locationName: "Default Location",
         peopleGoing: 1,
         maxPeople: 10,
+        remainingSpaces: (maxPeople - peopleGoing),
         price: "FREE"
     };    
 
     let showSpotsLeft = false;
-
     if(event.maxPeople > 0){
-        if(event.maxPeople - event.peopleGoing < Math.floor((event.maxPeople * 0.1) + 5)){
+        if(event.remainingSpaces < Math.floor((event.maxPeople * 0.1) + 5)){
             showSpotsLeft = true;
         }
     }
+
     
 </script>
 
@@ -27,7 +28,7 @@
 
         <!-- Image -->
         <div class="md:flex-shrink-0">
-            <a href="/"><img class="w-full h-40 hover:h-48 object-cover rounded-lg rounded-b-none" src="{event.imgURL}" alt="{event.imgAlt}"></a>
+            <a href="/"><img class="card-img" src="{event.imgURL}" alt="{event.imgAlt}"></a>
         </div>
         <div class="px-4 py-2">
 
@@ -35,15 +36,29 @@
             <a href="/"><h2 class="font-bold text-2xl text-gray-800 tracking-normal">{event.title}</h2></a>
             
             <!-- Time and headline -->
-            <p class="text-sm text-gray-700">
+            <p class="text-sm text-gray-700 mb-1">
                 {event.eventTime} - {event.category} at  {event.locationName}.
             </p>
 
             <!-- People attending and price -->
-            <div class="flex items-center justify-between">
-                <span class="text-gray-700">{event.peopleGoing} going {#if showSpotsLeft}<span class="text-black">-</span> <span class="text-red-600">{ event.maxPeople - event.peopleGoing} { ((event.maxPeople - event.peopleGoing) < 2) ? 'space' : 'spaces' } left!</span>{/if}</span>
-                <p class="bg-rashekgreen block text-center text-rashekblack font-bold px-3 py-1 rounded-lg mb-2">{event.price}</p>
+            <div class="flex items-center justify-between mb-2">
+                <span class="text-gray-700">{event.peopleGoing} going</span> 
+                {#if showSpotsLeft}
+                    <span class="text-black">-</span> 
+                    <span class="text-red-600">{ event.remainingSpaces } { (event.remainingSpaces < 2) ? 'space' : 'spaces' } left!</span>
+                {/if}
+                
+                <p class="card-price">{event.price}</p>
             </div>
         </div>
     </div>
 </div>
+
+<style>
+    .card-img {
+        @apply w-full h-40 object-cover rounded-lg rounded-b-none;
+    }
+    .card-price {
+        @apply bg-rashekgreen block text-center text-rashekblack font-bold px-3 py-1 rounded-lg;
+    }
+</style>
--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -2,7 +2,8 @@
     export let segment;
 </script>
 
-{#if segment}
+<!-- Pages not to show nav -->
+{#if segment != undefined && segment != "signup" && segment != "login"}
 
 <div class="bg-offwhite fixed bottom-0 w-full border-t border-gray-200 flex">
     <!-- Today -->
@@ -16,7 +17,7 @@
     </a>
 
     <!-- Favorites -->
-    <a href="/favorites" class="link">
+    <a href="/favorites" class="link" class:selected={segment === "favorites"}>
         <div>
             <svg class="svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
             <span class="link-text">Favorites</span>
@@ -24,7 +25,7 @@
     </a>
 
     <!-- Messages -->
-    <a href="/messages" class="link">
+    <a href="/messages" class="link" class:selected={segment === "messages"}>
         <div>
             <svg class="svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="comment-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M448 0H64C28.7 0 0 28.7 0 64v288c0 35.3 28.7 64 64 64h96v84c0 9.8 11.2 15.5 19.1 9.7L304 416h144c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64z"></path></svg>
             <span class="link-text">Messages</span>
@@ -32,7 +33,7 @@
     </a>
 
     <!-- Profile -->
-    <a href="/profile" class="link">
+    <a href="/profile" class="link" class:selected={segment === "profile"}>
         <div>
             <svg class="svg" aria-hidden="true" data-prefix="fas" data-icon="user" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"/></svg>
             <span class="link-text">Profile</span>
